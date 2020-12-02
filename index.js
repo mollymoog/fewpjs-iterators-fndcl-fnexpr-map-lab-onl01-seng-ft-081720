451879636aa854661b3ce1b9da923b31c7b9aa33
mollymoog <mmkover@gmail.com>
2020-12-02 11:10:07 -0500

    commit tree 4972fd1dc62b62e15307cf1c060ef2ff2f2e950c

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map( title => {
+    let words = title.split(" ");
+    let capitalWords = words.map( word => word.charAt(0).toUpperCase() + word.substr(1) ).join(" ");
+    return capitalWords
+  })
 }
